bf9cf9969b2a624c2c9d06f9dde9871c624350ed
Vitalik <vitalik.bb@ukr.net>
2021-10-31 18:40:21 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, {useState , useEffect} from 'react';
+import {Document , pdfjs, Page} from 'react-pdf';
 import './App.css';
+import file from './documents/file.pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function App() {
+const App = ()=>{
+  //const [base64 , setBase64] = useState('')
+
+  /*useEffect(()=>{
+
+    const readAndUploadFile = async ()=>{
+
+      const result = await new Promise((resolve, reject)=>{
+        const reader = new FileReader()
+        const blob = new Blob([ file ], {type: `application/pdf`})
+        
+        reader.readAsDataURL(blob)
+        
+              reader.addEventListener('load', ()=>{
+                  resolve({
+                      body: reader.result,
+                      isError: false
+                  })
+              })
+
+              reader.addEventListener('error', ()=>{
+                  reject({
+                      body: reader.result,
+                      isError: true,
+                      
+                  })
+              })
+
+      })
+      setBase64(result.body)
+    }
+     readAndUploadFile()
+
+  }, [])*/
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {
+                true  ? 
+                  <Document
+                    className={['document']}
+                    renderMode='svg'
+                    file={file}
+                  >
+                    <Page pageNumber={1}/>
+                  </Document>
+                          :
+                  <div>Loading...</div>
+           
+      }
     </div>
   );
 }
